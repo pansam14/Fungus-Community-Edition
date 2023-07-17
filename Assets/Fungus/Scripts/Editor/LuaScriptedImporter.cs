e334109aa0a31f336bbe05403873e5e75d1c7e9a
Evan Sklarski <esklarski@gmail.com>
2023-07-16 21:39:52 -0700

    Add directive to correctly import AssetImporters class.

--- Assets/Fungus/Scripts/Editor/LuaScriptedImporter.cs
+++ Assets/Fungus/Scripts/Editor/LuaScriptedImporter.cs
@@ -2,8 +2,13 @@
 // It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
 
 using UnityEngine;
-using UnityEditor.Experimental.AssetImporters;
 using System.IO;
+
+#if UNITY_2020_2_OR_NEWER
+	using UnityEditor.AssetImporters;
+#else
+	using UnityEditor.Experimental.AssetImporters;
+#endif
 
 namespace Fungus.EditorUtils
 {
