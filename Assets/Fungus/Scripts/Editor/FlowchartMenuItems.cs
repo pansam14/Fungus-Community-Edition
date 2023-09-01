4a80d79f076d14267ab11b1604797172ad6a42db
Evan Sklarski <esklarski@gmail.com>
2023-09-01 11:15:24 -0700

    Move package export menu items into own file. They're not a "Flowchart" item.

--- Assets/Fungus/Scripts/Editor/FlowchartMenuItems.cs
+++ Assets/Fungus/Scripts/Editor/FlowchartMenuItems.cs
@@ -36,29 +36,6 @@ namespace Fungus.EditorUtils
             SpawnPrefab("FungusLogo");
         }
 
-        [MenuItem("Tools/Fungus/Utilities/Export Fungus Package")]
-        static void ExportFungusPackageFull()
-        {
-            ExportFungusPackage( new string[] {"Assets/Fungus", "Assets/FungusExamples" });
-        }
-
-        [MenuItem("Tools/Fungus/Utilities/Export Fungus Package - Lite")]
-        static void ExportFungusPackageLite()
-        {
-            ExportFungusPackage(new string[] { "Assets/Fungus" });
-        }
-
-        static void ExportFungusPackage(string[] folders)
-        {
-            string path = EditorUtility.SaveFilePanel("Export Fungus Package", "", "Fungus", "unitypackage");
-            if (path.Length == 0)
-            {
-                return;
-            }
-
-            AssetDatabase.ExportPackage(folders, path, ExportPackageOptions.Recurse);
-        }
-
         public static GameObject SpawnPrefab(string prefabName)
         {
             GameObject prefab = Resources.Load<GameObject>("Prefabs/" + prefabName);
